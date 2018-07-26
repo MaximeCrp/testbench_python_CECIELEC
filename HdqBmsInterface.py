ba0d7e315ff62ffa9af2fcda16760d5f65bee160
Maxime Crampon <maxime.crampon@gmail.com>
2018-07-26 14:57:38 +0200

    add class for interface with BMS over HDQ

---
+++ HdqBmsInterface.py
@@ -0,0 +1,41 @@
+from mySerial import Hdq
+
+class HdqBmsInterface():
+
+    def __init__(self, cmd_current = [0x10, 0x11], cmd_voltage = [0x08, 0x09], cmd_i2c = [0x00, 0xe7, 0x29]):
+        self.hdq = Hdq("BMS HDQ")
+        self.cmd_current = cmd_current
+        self.cmd_voltage = cmd_voltage
+        self.cmd_i2c = cmd_i2c
+        
+    def read_voltage(self):
+        self.hdq.open_serial()
+        b1 = self.hdq.read_reg(self.cmd_voltage[0])
+        b2 = self.hdq.read_reg(self.cmd_voltage[1])
+        bytearr = bytes([b1, b2])
+        value = Hdq.uint16le(b1, b2)
+        print("value: 0x%04X" % value)
+        self.hdq.close_serial()
+        return(int.from_bytes(bytearr, byteorder = 'little', signed = False))
+
+    def read_current(self):
+        self.hdq.open_serial()
+        b1 = self.hdq.read_reg(self.cmd_current[0])
+        b2 = self.hdq.read_reg(self.cmd_current[1])
+        bytearr = bytes([b1, b2])
+        value = Hdq.uint16le(b1, b2)
+        print("value: 0x%04X" % value)
+        self.hdq.close_serial()
+        return(int.from_bytes(bytearr, byteorder = 'little', signed = True))
+
+
+        
+        """
+        write_reg(0x00, 0xe7)
+        write_reg(0x01, 0x29)
+        """
+        """ 
+        def read_reg(self, reg, size = 1):
+            self.write('\x00\xc0\xc0\xC0\xFE\xc0\xc0\xC0'.encode())
+            print(self.read(16))
+        """
